b2847714de9f8914df81d6bb01a529f2fe8747a2
ankitmukhia <ankitmukhia157@gmail.com>
2025-11-30 19:32:30 +0530

    addes metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
-// import localFont from "next/font/local";
 import "./globals.css";
-
-/* const roboto = localFont({
-  src: [
-    {
-      path: './Roboto-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './Roboto-Italic.woff2',
-
-      weight: '400',
-
-      style: 'italic',
-    },
-    {
-      path: './Roboto-Bold.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: './Roboto-BoldItalic.woff2',
-      weight: '700',
-      style: 'italic',
-    },
-  ],
-}) */
 
 const inter = Inter({
   variable: "--font-inter",
@@ -38,8 +10,30 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Fractal Wallpaper",
-  description: "Beutiful customizable Fractal Wallpepper.",
+  title: "Fractal Wallpaper | Create fractal wallpapers",
+  description: "A super simple tool to create fractal wallpapers.",
+  openGraph: {
+    title: "Fractal Wallpaper | Create fractal wallpapers",
+    description: "A super simple tool to create fractal wallpapers.",
+    type: "website",
+    siteName: "Fractal Wallpaper",
+    url: "https://fractal-wallpaper.vercel.app",
+    locale: "en_US",
+    images: [
+      {
+        url: "/open-graph-fractal.png",
+        width: 1200,
+        height: 630,
+        alt: "Fractal Wallpaper",
+      },
+    ],
+  },
+  twitter: {
+    title: "Fractal Wallpaper | Create fractal wallpapers",
+    description: "A super simple tool to create fractal wallpapers.",
+    card: "summary_large_image",
+    images: ["/twitter-fractal.png"],
+  },
 };
 
 // Wrape it with Wrapper component where we have custome wait logic besend on that we render main page or splash page.
